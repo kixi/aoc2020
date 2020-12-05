a26c26f43bc75041ecbc43468e6d813686e47ac6
guenter <4116520+kixi@users.noreply.github.com>
2020-12-05 06:50:16 +0100

    day 5

---
+++ src/aoc/d05.clj
@@ -0,0 +1,61 @@
+(ns aoc.d05
+  (:require [clojure.string :as str]
+            [aoc.cmn :as cmn]
+            [clojure.java.io :as io]))
+
+
+
+
+(def ex-seat "FBFBBFFRLR")
+
+(defn get-row-cols [seat]
+  (let [[_ row col] (re-find #"^([FB]{7})([LR]{3})$" seat)]
+    [row col]))
+
+(get-row-cols ex-seat)
+
+(defn bin [bs z o]
+  (let [s (-> bs
+              (str/replace z "0")
+              (str/replace o "1"))]
+    (Integer/parseInt s 2)))
+
+(defn seat-id [seat]
+  (let [[row col] (get-row-cols seat)]
+    (+
+       (* (bin row "F" "B") 8)
+       (bin col "L" "R"))))
+
+(seat-id ex-seat)
+;; star 1
+(->>
+  (cmn/slurp-lines "d05.txt")
+  (map seat-id)
+  (apply max))
+
+;; star 2
+(let [seats
+      (->>
+       (cmn/slurp-lines "d05.txt")
+       (map seat-id)
+       sort)]
+    (->>
+     (map (fn [s1 s2] [s1 s2 (- s2 s1)]) seats (rest seats))
+     (filter (fn [[s1 _ d]] (= d 2)))
+     (map (fn [[s1 _ _]] s1))
+     first
+     inc))
+
+;; star 2 reduce
+(let [seats
+      (->>
+       (cmn/slurp-lines "d05.txt")
+       (map seat-id)
+       sort)]
+  (->>
+   (reduce (fn [last curr]
+             (if (= (- curr last) 2)
+               (reduced (inc last))
+               curr))
+           (first seats)
+           (rest seats))))
