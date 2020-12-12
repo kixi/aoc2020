f75c3a492fc837ad49ff1ad88409f2240e66534b
guenter <4116520+kixi@users.noreply.github.com>
2020-12-12 14:55:22 +0100

    day 11

--- src/aoc/d11.clj
+++ src/aoc/d11.clj
@@ -6,7 +6,8 @@
             [clojure.math.combinatorics :as c]))
 
 (def demo
-  "L.LL.LL.LL
+  (str/split-lines
+   "L.LL.LL.LL
 LLLLLLL.LL
 L.L.L..L..
 LLLL.LL.LL
@@ -15,19 +16,39 @@ L.LLLLL.LL
 ..L.L.....
 LLLLLLLLLL
 L.LLLLLL.L
-L.LLLLL.LL")
+L.LLLLL.LL"))
+
 (def demo (cmn/slurp-lines "d11.txt"))
+
 (def wa0
   (->> demo
        (mapv #(into [] %))
        (into []))
   )
 
+(def directions
+  (for [x [-1 0 1] y [-1 0 1] :when (not= x y 0)]
+    [x y]))
+
+(defn v+ [v0 v1]
+  (mapv + v0 v1))
+
+(defn log [x]
+  (prn x)
+  x)
+
+(defn rc [pv]
+  [(second pv) (first pv)])
+
+(defn find-move
+  [wa pv dir]
+  (let [rv (first  (drop-while #(= \. (get-in wa (rc %))) (rest (iterate (partial  v+ dir) pv))))] ;
+    (when (get-in wa (rc rv)) rv)))
+
 (defn neighbours [waiting-area px py]
   (let [height (count waiting-area)
         width (count (first waiting-area))
-        moves (for [x [-1 0 1] y [-1 0 1] :when (not= x y 0)]
-                [(+ px x) (+ py y)])]
+        moves (map (partial v+ [px py]) directions)]
     (->>
      moves
      (filter (fn [[x y]] (and (<= 0 x (dec width))
@@ -35,6 +56,15 @@ L.LLLLL.LL")
 
              )
      (map #(get-in waiting-area [(second  %) (first %)])))))
+
+(defn neighbours [waiting-area px py]
+  ( let [height (count waiting-area)
+         width (count (first waiting-area))
+         moves (keep #(find-move waiting-area [px py] %)   directions)]
+   (->>
+    moves
+    (filter identity)
+    (map #(get-in waiting-area [(second  %) (first %)])))))
 
 
 (defn calc-seat [waiting-area x y]
@@ -46,7 +76,7 @@ L.LLLLL.LL")
           (and (= seat \L) (not-any? #(= % \#) n))
           \#
 
-          (and (= seat \#) (>=  (get (frequencies n) \# 0) 4))
+          (and (= seat \#) (>=  (get (frequencies n) \# 0) 5))
           \L
 
           :else
@@ -55,18 +85,19 @@ L.LLLLL.LL")
   )
 
 (defn step [wa]
-  (reduce (fn [wa* [x y]]
-            (assoc-in wa* [y x] (calc-seat wa x y))
-            )
-          wa
-          (for [x (range 0 (count (first wa)))
-                y (range 0 (count wa))]
-            [x y])))
+(reduce (fn [wa* [x y]]
+          (assoc-in wa* [y x] (calc-seat wa x y))
+          )
+        wa
+        (for [x (range 0 (count (first wa)))
+              y (range 0 (count wa))]
+          [x y])))
 
-(def wa1 (step wa0))
-(def wa2 (step wa1))
+(def wa1 (log  (step wa0)))
+(def wa2 (log  (step wa1)))
+
+(neighbours wa1 0 2)
 (calc-seat wa1 3 0)
-(neighbours wa1 3 0)
 (defn log [x] (clojure.pprint/pprint x) x)
 (->> wa0
      (iterate step)
@@ -77,7 +108,3 @@ L.LLLLL.LL")
      flatten
      (filter #(= % \#))
      count)
-
-
-
-(clojure.pprint/pprint *1)
